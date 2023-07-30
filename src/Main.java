524137d06ab18be43778ee081471cc16c5c11c08
Roman Mudrud <Mudrudr@gmail.com>
2023-07-30 12:55:30 +0300

    change encryption in main

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
         int start = 0;
         Path encryptPath = Path.of("C:\\Users\\Roman\\IdeaProjects\\CryptedText\\EncryptionText.txt");
         Path pathDict = Path.of("C:\\Users\\Roman\\IdeaProjects\\CryptedText\\dictionary.txt");
+        Path keyPath = Path.of("C:\\Users\\Roman\\IdeaProjects\\CryptedText\\key.txt");
         while ( choice != 0) {
             System.out.println("Select the program operation mode:");
             System.out.println("If you want to encrypt the file enter: 1");
@@ -22,6 +23,8 @@ public class Main {
             String encryptionText;
             switch (choice) {
                 case 1:
+                    Files.delete(keyPath);
+                    Files.createFile(keyPath);
                     Files.delete(encryptPath);
                     Files.createFile(encryptPath);
                     System.out.println("Enter the path to the text file");
@@ -29,47 +32,33 @@ public class Main {
                     String path = scanner1.nextLine();
                     Path textPath = Path.of(path);
                     System.out.println("Enter key");
-                    object = new Encryption(scanner.nextInt());
+                    int decryptionkey = scanner.nextInt();
+                    object = new Encryption(decryptionkey);
                     System.out.println("Encryption text");
                     System.out.println();
                     encryptionText = object.encryptionMethod(Files.readString(textPath));
                     Files.write(encryptPath, encryptionText.getBytes(StandardCharsets.UTF_8));
+                    Files.write(keyPath, String.valueOf(decryptionkey).getBytes());
                     System.out.println(encryptionText);
                     break;
                 case 2:
-                    System.out.println("If you have already encrypted the text, enter: 1");
-                    System.out.println("For a new encryption enter: 2");
-                    int c = scanner.nextInt();
-                    switch (c) {
-                        case 1:
-                            System.out.println("Enter the path to the encrypted text");
-                            Scanner scanner2 = new Scanner(System.in);
-                            String StringPathEncrypt = scanner2.nextLine();
-                            Path path1 = Path.of(StringPathEncrypt);
-                            System.out.println("Enter key");
-                            int key3 = scanner2.nextInt();
-                            System.out.println("Decryption text");
-                            System.out.println();
-                            object = new Encryption(key3);
-                            System.out.println(object.decryptionMethod(Files.readString(path1)));
-                            break;
-                        case 2:
-                            System.out.println("Enter the path to the text");
-                            Scanner scanner3 = new Scanner(System.in);
-                            String pathToText = scanner3.nextLine();
-                            Path path2 = Path.of(pathToText);
-                            System.out.println("Enter the key for encryption");
-                            int key2 = scanner.nextInt();
-                            System.out.println("Decryption text");
-                            System.out.println();
-                            object = new Encryption(key2);
-                            System.out.println(object.decryptionMethod(object.encryptionMethod(Files.readString(path2))));
-                            Files.delete(encryptPath);
-                            Files.createFile(encryptPath);
-                            Files.write(encryptPath,object.encryptionMethod(Files.readString(path2)).getBytes(StandardCharsets.UTF_8));
-                            break;
+                    if(Files.readString(encryptPath).length() != 0) {
+                        System.out.println("Enter the path to the encrypted text");
+                        Scanner scanner2 = new Scanner(System.in);
+                        String StringPathEncrypt = scanner2.nextLine();
+                        Path path1 = Path.of(StringPathEncrypt);
+                        int key3 = Integer.parseInt(Files.readString(keyPath));
+                        System.out.println("Decryption text");
+                        System.out.println();
+                        object = new Encryption(key3);
+                        System.out.println(object.decryptionMethod(Files.readString(path1)));
+                    }else {
+                        System.out.println("Encrypted file is empty");
+                        System.out.println("You must decrypt the file");
+                        System.out.println();
                     }
                     break;
+
                 case 3:
                     System.out.println("Brute Force Method");
                     System.out.println("Enter maximum volume of key");
@@ -89,7 +78,7 @@ public class Main {
                         System.out.println("Wait please...");
                         bruteForce.force(Files.readString(path1), start, key, size, pathDict, firstLetterString.charAt(0));
                         System.out.println("Everything is OK ?");
-                        System.out.println("If the text is not accurate enter 0, else enter 1");
+                        System.out.println("If the text is not accurate enter 1, else enter 0");
                         start = bruteForce.returnKey() + 1;
                         ok = scanner.nextInt();
                     }
@@ -98,6 +87,5 @@ public class Main {
             }
         }
     }
-
 
 }
